8e1079112c7f92159e0e5dc8268aae9b880d9299
redskullvue <pouya.9996@gmail.com>
2024-10-29 15:23:00 +0330

    Feat : Service Cards Added

---
+++ src/components/serviceCard.vue
@@ -0,0 +1,22 @@
+<template>
+  <div
+    class="md:w-1/3 md:h-[400px] w-[90%] border border-dashed border-gray-400 flex items-center justify-center flex-col p-4 rounded-lg"
+  >
+    <img src="../assets/images/bag-logo.png" alt="bag-logo" class="mb-4" />
+    <h3 class="mb-4 text-xl font-bold">{{ cardInfo.title }}</h3>
+    <p class="text-gray-400 text-center text-lg">
+      {{ cardInfo.description }}
+    </p>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  cardInfo: {
+    type: Object,
+    required: true,
+  },
+});
+</script>
+
+<style scoped></style>
