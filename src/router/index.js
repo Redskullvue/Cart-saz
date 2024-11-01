c4419f5629ac4ccdd65532b7c9c00518f11affca
redskullvue <pouya.9996@gmail.com>
2024-11-01 10:11:56 +0330

    Feat : NotFoundPage Added

--- src/router/index.js
+++ src/router/index.js
@@ -4,10 +4,16 @@ import defaultLayout from "@/components/defaultLayout.vue";
 import ContactUsView from "@/views/ContactUsView.vue";
 import FAQView from "@/views/FAQView.vue";
 import loginView from "@/views/loginView.vue";
+import notFoundView from "@/views/notFoundView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    {
+      path: "/:catchAll(.*)",
+      name: "NotFound",
+      component: notFoundView,
+    },
     {
       path: "/",
       component: defaultLayout,
