85aa554f9a8af8bdb7c7bb431d99c7fcf3c55fd6
redskullvue <pouya.9996@gmail.com>
2024-11-01 10:13:52 +0330

    Fix : Styling issue + Added new route

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,14 +4,14 @@
       <img src="../assets/images/logo.svg" alt="logo" />
     </router-link>
 
-    <div class="w-2/3 flex text-lg justify-center">
+    <div class="w-2/3 flex text-lg justify-end md:justify-center">
       <div class="hidden md:flex items-center justify-end gap-8 w-full">
         <router-link to="/">وبلاگ</router-link>
         <router-link to="/faq">سوالات متداول</router-link>
         <router-link to="/تماس-با-ما">تماس با ما</router-link>
       </div>
       <router-link
-        to="/"
+        to="/login"
         class="mr-0 md:mr-14 text-blue-400 border border-blue-400 py-2 px-4 rounded-lg min-w-[150px] flex items-center justify-center transition-colors duration-300 hover:bg-blue-500 hover:text-white"
         >ورود یا ثبت نام</router-link
       >
