ae00bc60c66985c05fc495d0ccd34fa8a687f31c
redskullvue <pouya.9996@gmail.com>
2024-11-03 16:21:09 +0330

    Feat : admin Profile page complete ui

--- src/views/shopAdminProfile.vue
+++ src/views/shopAdminProfile.vue
@@ -1,5 +1,70 @@
 <template>
-  <div>This is the shop admin profile settings page</div>
+  <div class="w-full">
+    <div
+      class="w-full grid grid-cols-1 md:grid-cols-3 gap-4 p-4 border-b border-gray-300"
+    >
+      <!-- Name -->
+      <div>
+        <h4 class="mb-4 text-sm md:text-base text-gray-600">نام شما</h4>
+        <h2 class="md:text-xl">احسان عزتی</h2>
+      </div>
+
+      <button
+        class="w-32 absolute py-2 text-sm rounded-lg text-white bg-black left-4 mb-10"
+      >
+        ویرایش اطلاعات
+      </button>
+      <!-- Phone number -->
+      <div>
+        <h4 class="mb-4 text-sm md:text-base text-gray-600">شماره تماس</h4>
+        <h2 class="md:text-xl">۰۹۱۰۲۲۳۲۳۷۸</h2>
+      </div>
+      <!-- Email -->
+      <div>
+        <h4 class="mb-4 text-sm md:text-base text-gray-600">ایمیل</h4>
+        <h2 class="md:text-xl">pooyaworkjs@gmail.com</h2>
+      </div>
+    </div>
+
+    <div
+      class="w-full grid grid-cols-1 md:grid-cols-3 gap-4 p-4 border-b border-gray-300"
+    >
+      <!-- shop name -->
+      <div>
+        <h4 class="mb-4 text-sm md:text-base text-gray-600">نام فروشگاه</h4>
+        <h2 class="md:text-xl">فروشگاه هگزاگون</h2>
+      </div>
+      <!-- shop's instagram -->
+      <div>
+        <h4 class="mb-4 text-sm md:text-base text-gray-600">
+          اینستاگرام فروشگاه
+        </h4>
+        <h2 class="md:text-xl">instagram.com/hexagon</h2>
+      </div>
+      <!-- shop's ethic -->
+      <div>
+        <h4 class="mb-4 text-sm md:text-base text-gray-600">حوزه فروشگاه</h4>
+        <h2 class="md:text-xl">مد و لباس</h2>
+      </div>
+    </div>
+
+    <div
+      class="w-full grid grid-cols-1 md:grid-cols-3 gap-4 p-4 border-b border-gray-300 mb-4"
+    >
+      <!-- shop name -->
+      <div>
+        <h4 class="mb-4 text-sm md:text-base text-gray-600">آدرس فرستنده</h4>
+        <h2 class="md:text-xl">----</h2>
+      </div>
+      <!-- shop's instagram -->
+      <div>
+        <h4 class="mb-4 text-sm md:text-base text-gray-600">
+          کد ارسال فرستنده
+        </h4>
+        <h2 class="md:text-xl">----</h2>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup></script>
