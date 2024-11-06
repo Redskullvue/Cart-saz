cbbc083e8f4be1efed708ab768eab05b623f167b
redskullvue <pouya.9996@gmail.com>
2024-11-06 16:05:00 +0330

    Feat : Toggle Button Component Added

---
+++ src/components/ToggleButton.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div
+      @click="toggle = !toggle"
+      class="rounded-full w-14 h-7 p-1 flex flex-row-reverse items-center justify-between cursor-pointer"
+      :class="toggle ? 'bg-blue-600' : 'bg-gray-300'"
+    >
+      <div
+        class="w-6 h-6 rounded-full border bg-white transition-all duration-300"
+        :class="toggle ? ' translate-x-6' : ''"
+      ></div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const toggle = ref(true);
+</script>
