9ef99184b95c0ea63cee91edfc6498c28ef187e5
redskullvue <pouya.9996@gmail.com>
2024-11-08 09:00:21 +0330

    Feat : Vitrin Layout Added

---
+++ src/components/vitrinLayout.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="w-full">
+    <header
+      class="w-full bg-blue-500 h-32 rounded-b-xl p-4 flex gap-3 items-center text-white"
+    >
+      <div>
+        <img src="../assets/images/vitrin-logo.png" alt="vitrin-logo" />
+      </div>
+      <div>
+        <h2 class="text-xl mb-3">فروشگاه کارتساز</h2>
+        <p class="text-gray-300 text-sm">فروش لباس های مجلسی</p>
+      </div>
+    </header>
+    <main>
+      <router-view></router-view>
+    </main>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
---
+++ src/views/UserVitrinView.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>this is what user will see when qr code scanned</div>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
