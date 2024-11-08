037f183bb9bcec09b3bf1adc89586f1430a98e7c
redskullvue <pouya.9996@gmail.com>
2024-11-08 09:21:08 +0330

    feat : UserVitrin View Completed

---
+++ src/components/vitrinProductCard.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="w-full h-[300px] rounded-lg border border-gray-300 p-2">
+    <div class="w-full flex items-center justify-center">
+      <img src="../assets/images/vitrin-product.png" alt="Vitrin-product" />
+    </div>
+    <div class="mt-4 border-t text-base border-gray-300 py-3">
+      <h4 class="text-gray-600">لباس مخصوص کوهنوردی</h4>
+      <p class="w-full text-green-500 mt-4">۲۵۰.۰۰۰ تومان</p>
+    </div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
