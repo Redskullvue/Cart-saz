6cd757a6179f0dc6320c347e45584f01a72f7c0e
redskullvue <pouya.9996@gmail.com>
2024-11-09 16:35:59 +0330

    Feat : Level 2 For user Cart Signup added

--- src/views/userCartView.vue
+++ src/views/userCartView.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="w-full p-6 flex items-center justify-center flex-col">
+  <div
+    class="w-full p-6 flex items-center justify-center flex-col"
+    v-if="currentLevel === 1"
+  >
     <div
       class="flex md:w-[60%] items-center justify-center flex-col border-b border-gray-300 w-full py-3"
     >
@@ -41,11 +44,59 @@
     </div>
 
     <div class="w-full flex items-center justify-center mt-6 md:w-[40%]">
-      <button class="w-full rounded-lg py-3 px-1 text-white bg-blue-500">
+      <button
+        class="w-full rounded-lg py-3 px-1 text-white bg-blue-500"
+        @click="currentLevel = 2"
+      >
         تکمیل خرید
       </button>
     </div>
   </div>
+  <div
+    class="flex w-[100%] h-screen flex-col p-2 items-center justify-center mt-2"
+    v-if="currentLevel === 2"
+  >
+    <h2 class="text-blue-500 text-2xl md:text-3xl font-bold">
+      فروشگاه هگزاگون
+    </h2>
+    <p class="mt-4">کد وارد شده به شماره ۰۹۱۰۲۲۳۲۳۷۸ را وارد کنید</p>
+    <div class="w-full flex md:items-center md:justify-center mt-7">
+      <input
+        type="text"
+        class="border border-gray-500 w-[15%] rounded-lg h-16 md:h-28 mx-2"
+        maxlength="1"
+      />
+      <input
+        type="text"
+        maxlength="1"
+        class="border border-gray-500 w-[15%] rounded-lg h-16 md:h-28 mx-2"
+      />
+      <input
+        type="text"
+        maxlength="1"
+        class="border border-gray-500 w-[15%] rounded-lg h-16 md:h-28 mx-2"
+      />
+      <input
+        type="text"
+        maxlength="1"
+        class="border border-gray-500 w-[15%] rounded-lg h-16 md:h-28 mx-2"
+      />
+      <input
+        type="text"
+        maxlength="1"
+        class="border border-gray-500 w-[15%] rounded-lg h-16 md:h-28 mx-2"
+      />
+    </div>
+    <div class="w-full flex items-center justify-center mt-8 md:w-[40%]">
+      <button
+        class="w-full rounded-lg py-3 px-1 text-white bg-blue-500"
+        @click="currentLevel = 3"
+      >
+        ثبت کد
+      </button>
+    </div>
+  </div>
+  <div class="w-full" v-if="currentLevel === 3">This is the level 3</div>
 </template>
 
 <script setup>
@@ -69,6 +120,8 @@ const products = ref([
     img: "product1.png",
   },
 ]);
+
+const currentLevel = ref(1);
 </script>
 
 <style scoped></style>
