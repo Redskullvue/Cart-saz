a81b873e7a4b2d635749b835089c14112fd6d2be
redskullvue <pouya.9996@gmail.com>
2024-11-09 17:47:27 +0330

    Fix : Removed Persian Routes

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -8,7 +8,7 @@
       <div class="hidden md:flex items-center justify-end gap-8 w-full">
         <router-link to="/">وبلاگ</router-link>
         <router-link to="/faq">سوالات متداول</router-link>
-        <router-link to="/تماس-با-ما">تماس با ما</router-link>
+        <router-link to="/contact-us">تماس با ما</router-link>
       </div>
       <router-link
         to="/login"
