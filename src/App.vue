0474e17b3b02e26600c3a4f6a05a511faf1879c9
redskullvue <pouya.9996@gmail.com>
2024-11-12 11:19:29 +0330

    Fix : Now Application will check local for user auth too

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,17 @@
   <router-view />
 </template>
 
+<script setup>
+import { onMounted } from "vue";
+import { useCounterStore } from "./stores/shoppers";
+
+const store = useCounterStore();
+
+onMounted(() => {
+  store.isUserAuthed = JSON.parse(localStorage.getItem("isAuthed"));
+});
+</script>
+
 <style>
 * {
   font-family: "morabba", sans-serif;
--- src/stores/shoppers.js
+++ src/stores/shoppers.js
@@ -1,4 +1,4 @@
-import { ref, computed } from "vue";
+import { ref } from "vue";
 import { defineStore } from "pinia";
 
 export const useCounterStore = defineStore("shoppers", () => {
@@ -24,6 +24,7 @@ export const useCounterStore = defineStore("shoppers", () => {
   }
   function authUser() {
     isUserAuthed.value = true;
+    localStorage.setItem("isAuthed", isUserAuthed.value);
   }
   return {
     shopOwnerInformation,
