8750e2effd50a7f3a47975360ae58d6abb13c4ff
redskullvue <pouya.9996@gmail.com>
2024-11-12 15:17:20 +0330

    Feat : Connected buttons to their routes

--- src/views/shopAdminMain.vue
+++ src/views/shopAdminMain.vue
@@ -1,8 +1,12 @@
 <template>
   <div class="w-full h-full mb-4">
     <div class="w-full border-b border-gray-300">
-      <h2 class="text-4xl text-blue-500">فروشگاه هگزاگون</h2>
-      <h4 class="text-gray-600 mt-2 mb-3">محمد شکروی خوش آمدید</h4>
+      <h2 class="text-4xl text-blue-500">
+        فروشگاه {{ store.shopOwnerInformation.shopName }}
+      </h2>
+      <h4 class="text-gray-600 mt-2 mb-3">
+        {{ store.shopOwnerInformation.name }} خوش آمدید
+      </h4>
     </div>
     <div
       class="w-full flex flex-col md:flex-row justify-center mt-10 overflow-y-scroll"
@@ -29,7 +33,8 @@
           هیچ آیتمی برای نمایش وجود ندارد
         </p>
         <div class="w-full flex items-center justify-center mt-10">
-          <button
+          <router-link
+            to="/dashboard/carts"
             class="rounded-full w-56 py-3 bg-gray-100 flex justify-center gap-2"
             v-if="carts.length > 0"
           >
@@ -40,7 +45,7 @@
               height="23"
               style="color: black"
             />
-          </button>
+          </router-link>
         </div>
       </div>
 
@@ -63,7 +68,8 @@
           <p>۲۵۰.۰۰۰ تومان</p>
         </div>
         <div class="w-full flex items-center justify-center mt-10">
-          <button
+          <router-link
+            to="/dashboard/products"
             class="rounded-full w-56 py-3 bg-gray-100 flex justify-center gap-2"
           >
             مشاهده همه
@@ -73,7 +79,7 @@
               height="23"
               style="color: black"
             />
-          </button>
+          </router-link>
         </div>
       </div>
     </div>
@@ -85,6 +91,9 @@ import { Icon } from "@iconify/vue";
 import cartCard from "@/components/cartCard.vue";
 import { ref } from "vue";
 import { dateCreator } from "@/composables/dateCreator";
+import { useCounterStore } from "@/stores/shoppers";
+
+const store = useCounterStore();
 
 const carts = ref([
   {
