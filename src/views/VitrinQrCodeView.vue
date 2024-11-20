7d6b0289be13e6b1b728404fd27e60d6ed3f4282
redskullvue <pouya.9996@gmail.com>
2024-11-20 19:30:06 +0330

    Feat : Qr Code will be genrated dynamicly

--- src/views/VitrinQrCodeView.vue
+++ src/views/VitrinQrCodeView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="w-full">
     <div class="w-full flex items-center justify-center">
-      <img src="../assets/images/qrcode.png" alt="qr-code" />
+      <img :src="qrCodeUrl" alt="qr-code" />
     </div>
     <button
       class="bg-gray-200 rounded-lg py-3 px-1 w-full mt-6 flex items-center justify-center gap-2"
@@ -36,6 +36,30 @@
 
 <script setup>
 import { Icon } from "@iconify/vue";
+import QRCode from "qrcode";
+import { useCounterStore } from "@/stores/shoppers";
+const store = useCounterStore();
+
+let qrCodeUrl = null;
+var opts = {
+  errorCorrectionLevel: "H",
+  type: "image/jpeg",
+  quality: 0.3,
+  margin: 1,
+  color: {
+    dark: "#000000",
+    light: "#FFFFFF",
+  },
+};
+
+QRCode.toDataURL(
+  "https://cartsaz1.netlify.app/vitrin/" + store.shopOwnerInformation.shopId,
+  opts,
+  function (err, url) {
+    if (err) throw err;
+    qrCodeUrl = url;
+  }
+);
 </script>
 
 <style scoped></style>
