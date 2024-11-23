3ed1bb7617a651851679e243d34caf6f2ed6a212
redskullvue <pouya.9996@gmail.com>
2024-11-23 17:16:19 +0330

    Fix : Some Styling Issues

--- src/components/vitrinProductCard.vue
+++ src/components/vitrinProductCard.vue
@@ -4,8 +4,18 @@
     class="w-full h-[300px] rounded-lg border border-gray-300 p-2"
   >
     <div class="w-full flex items-center justify-center">
-      <img v-if="data.img" :src="imgUrl" alt="Vitrin-product" />
-      <img v-else src="../assets/images/producterr.png" alt="product-image" />
+      <img
+        v-if="data.img"
+        :src="imgUrl"
+        alt="Vitrin-product"
+        class="min-w-[130px] min-h-[130px]"
+      />
+      <img
+        v-else
+        src="../assets/images/producterr.png"
+        alt="product-image"
+        class="min-w-[130px] min-h-[130px]"
+      />
     </div>
     <div class="mt-4 border-t text-base border-gray-300 py-3">
       <h4 class="text-gray-600">{{ data.title }}</h4>
